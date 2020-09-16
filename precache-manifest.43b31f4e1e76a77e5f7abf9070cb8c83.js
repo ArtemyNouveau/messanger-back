0d386d30bf174abd6394ab2cfe12b5e77b0eb46e
artemyNouveau <artnovkov@gmail.com>
2020-09-16 12:47:01 +0300

    Updates

---
+++ precache-manifest.43b31f4e1e76a77e5f7abf9070cb8c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13f9737485bb82b5661f9efaf395916",
+    "url": "/messanger-front/index.html"
+  },
+  {
+    "revision": "067747c7b3d27f03baa9",
+    "url": "/messanger-front/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d056f34f424a8c4f3694",
+    "url": "/messanger-front/static/js/2.688337e9.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/messanger-front/static/js/2.688337e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067747c7b3d27f03baa9",
+    "url": "/messanger-front/static/js/main.74b45d61.chunk.js"
+  },
+  {
+    "revision": "1db3c35d51675b3eaeac",
+    "url": "/messanger-front/static/js/runtime-main.3f8902c6.js"
+  }
+]);
